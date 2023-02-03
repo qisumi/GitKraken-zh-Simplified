5aad5b1654826bb5b6feb2bc3de722ee2fd4e3ef
qisumi <940858763@qq.com>
2023-02-04 02:11:03 +0800

    完成部分翻译

---
+++ update.py
@@ -0,0 +1,7 @@
+from json import load, dump
+
+with open('./strings.en.json', encoding='utf8') as enJSON, open('./strings.json', encoding='utf8') as zhJSON:
+    data = load(enJSON) | load(zhJSON)
+
+with open('./strings.json', 'w', encoding='utf8') as JSON:
+    dump(data, JSON, indent=4, ensure_ascii=False)
